e04b5bd45988d9fbbc79d2c3b48581f88c94d630
Mert <m.mertgn@gmail.com>
2018-01-14 23:51:17 +0300

    Profil Resmi Update işlemi Ajax İle Yapıldı

---
+++ FashionStore-BLL/Services/Validations/ProfilUpdateValidator.cs
@@ -0,0 +1,30 @@
+﻿using FashionStore.Entity.Entities;
+using FluentValidation;
+
+namespace FashionStore_BLL.Services.Validations
+{
+    public class ProfilUpdateValidator : AbstractValidator<CustomerPicture>
+    {
+        //public IUnitOfWork _UnitOfWork;
+        public ProfilUpdateValidator()
+        {
+            RuleFor(x => x.Customer.Name).NotEmpty().WithMessage("İsim alanı boş bırakılamaz.");
+            RuleFor(x => x.Customer.Surname).NotEmpty().WithMessage("Soyad alanı boş bırakılamaz.");
+            RuleFor(x => x.Customer.Email)
+                .NotEmpty().WithMessage("Email alanı boş bırakılamaz.")
+                .EmailAddress().WithMessage("Lütfen e-posta adresinizi giriniz.")
+                .Must((x, y) => y != x.Customer.Email).WithMessage("Bu Email adresi zaten kayıtlı.");
+            RuleFor(x => x.Customer.Password)
+                .Length(6, 16).WithMessage("Şifreniz 6-16 karakter uzunluğunda olmalıdır.")
+                .NotEmpty().WithMessage("\"Şifre\" alanını boş bıraktınız."); ;
+            
+        }
+
+        private bool UniqueEmail(string o)
+        {
+            //var dbEmail = _UnitOfWork.GetRepo<CustomerPicture>().Where(x => x.Customer.Email == o).SingleOrDefault();
+            return true;
+        }
+        
+    }
+}
--- FashionStore.UI.Web/Areas/Admin/Controllers/CustomerController.cs
+++ FashionStore.UI.Web/Areas/Admin/Controllers/CustomerController.cs
@@ -1,15 +1,26 @@
-﻿using System.IO;
+﻿
+using System.IO;
 using System.Linq;
+using System.Web;
 using FashionStore.Repository.Repositories.Abstracts;
 using FashionStore.UI.Web.Controllers;
 using System.Web.Mvc;
 using FashionStore.Entity.Entities;
+using FashionStore_BLL.Services.Abstracts;
+using FashionStore_BLL.Services.Validations;
+using FluentValidation.Results;
+using Unity.Attributes;
 
 namespace FashionStore.UI.Web.Areas.Admin.Controllers
 {
     [Authorize]
     public class CustomerController : BaseController
     {
+        private readonly IEncryptor _encryptor;
+        public CustomerController(IUnitOfWork unitOfWork, [Dependency("MD5")]IEncryptor encryptor) : base(unitOfWork)
+        {
+            _encryptor = encryptor;
+        }
         public ActionResult List()
         {
             var model = _unitOfWork.GetRepo<Customer>().GetAll().ToList();
@@ -24,22 +35,65 @@ namespace FashionStore.UI.Web.Areas.Admin.Controllers
         [HttpPost]
         public ActionResult ProfileEdit(CustomerPicture model)
         {
-            if (model.Picture.PicturePath != null)
+
+            var validator = new ProfilUpdateValidator();
+            ValidationResult result = validator.Validate(model);
+            if (result.IsValid)
             {
-                string dosyaYolu = Path.GetFileName(model.Picture.PicturePath);
-                var yuklemeYeri = Path.Combine(Server.MapPath("~/Dosyalar"), dosyaYolu);
-                //model.Picture.PicturePath.SaveAs(yuklemeYeri);
+                model.Customer.Password = _encryptor.Hash(model.Customer.Password);
+                _unitOfWork.GetRepo<CustomerPicture>().Update(model);
+                _unitOfWork.Commit();
             }
+            //var m = new MessageResult
+            //{
+            //    ErrorMessage = result.Errors.Select(x => x.ErrorMessage).ToList(),
+            //    IsSucceed = result.IsValid
+            //};
+            //m.SuccessMessage = m.IsSucceed == true ? "." : "Hatalı bilgiler mevcut";
+
+
+
             return RedirectToAction("ProfileEdit");
         }
 
+        public JsonResult GetProfilPhoto()
+        {
+            var model = _unitOfWork.GetRepo<CustomerPicture>().Where(x => x.Customer.Email == HttpContext.User.Identity.Name).FirstOrDefault();
+            return Json(model.Picture.PicturePath, JsonRequestBehavior.AllowGet);
+        }
         [HttpPost]
-        public ActionResult Edit(Customer customer)
+        public JsonResult ProfilPhotoUpload(HttpPostedFileBase profilphoto)
         {
-            return View();
+            if (profilphoto == null) return Json("");
+
+            var picturePath = Path.GetFileName(profilphoto.FileName);
+            var model = _unitOfWork.GetRepo<CustomerPicture>().Where(x => x.Customer.Email == HttpContext.User.Identity.Name).FirstOrDefault();
+            model.Picture.PicturePath = picturePath;
+            _unitOfWork.GetRepo<CustomerPicture>().Update(model);
+            _unitOfWork.Commit();
+            var yuklemeYeri = Path.Combine(Server.MapPath("~/Uploads"), picturePath);
+            profilphoto.SaveAs(yuklemeYeri);
+            //Database e resim ekleme işlemi yap ve eklenen resmin adını döndür
+            ViewBag.Kontrol = true;
+            return Json(picturePath);
         }
-        public CustomerController(IUnitOfWork unitOfWork) : base(unitOfWork)
+        public JsonResult ProfilPhotoDelete()
         {
+            var model = _unitOfWork.GetRepo<CustomerPicture>().Where(x => x.Customer.Email == HttpContext.User.Identity.Name).FirstOrDefault();
+            var fullPath = Server.MapPath("~/Uploads/" + model.Picture.PicturePath);
+
+            if (model.Picture.PicturePath == "defaultprofil.jpg")
+                return Json(model.Picture.PicturePath, JsonRequestBehavior.AllowGet);
+
+            if (!System.IO.File.Exists(fullPath)) return Json(model.Picture.PicturePath, JsonRequestBehavior.AllowGet);
+
+            System.IO.File.Delete(fullPath);
+            model.Picture.PicturePath = "defaultprofil.jpg";
+            _unitOfWork.GetRepo<CustomerPicture>().Update(model);
+            _unitOfWork.Commit();
+            return Json(model.Picture.PicturePath, JsonRequestBehavior.AllowGet);
         }
+        
+
     }
 }
